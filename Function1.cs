f8ea6633e8c36150576c2cd67ac9c624860b3fe8
JP <herrerajohnpaul042090@gmail.com>
2023-07-16 02:17:16 +0800

    Update as of 07-16-2023

--- Function1.cs
+++ Function1.cs
@@ -22,36 +22,25 @@ namespace AzureFunctionText07152023
             log.LogInformation("C# HTTP trigger function processed a request.");
 
             var cosmosURL = "https://cosmodbnosql07152023.documents.azure.com:443/";
-            var cosmosKey = "Bz4Ld1RMEhtlrRDsnxSxBA0duoMRI0cXTS6Dt17ghQyg7wCTljsiR20HEltYUHLqK5CDSiDZiNhYACDbGkMHGQ==";
+            var cosmosKey = Environment.GetEnvironmentVariable("COSMOS_KEY");
             var databaseName = "CustomerDB";
+
+            if (string.IsNullOrEmpty(cosmosURL) || string.IsNullOrEmpty(cosmosKey))
+            {
+                return new BadRequestObjectResult("Cosmos DB credentials are missing.");
+            }
 
             try
             {
-                Customer cust = new Customer();
                 CosmosClient client = new CosmosClient(cosmosURL, cosmosKey);
                 Database db = await client.CreateDatabaseIfNotExistsAsync(databaseName);
-                Container cont = await db.CreateContainerIfNotExistsAsync(
-                    "Customer", "/id", 400);
+                Container cont = await db.CreateContainerIfNotExistsAsync("Customer", "/id", 400);
 
-                cust = new Customer()
-                {
-                    id = Guid.NewGuid().ToString(),
-                    FirstName = "Mark",
-                    LastName = "Monurst",
-                    BirthdayInEpoch = 125566,
-                    Email = "mark@gmail.com"
-                };
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var customer = JsonConvert.DeserializeObject<Customer>(requestBody);
+                customer.id = Guid.NewGuid().ToString();
 
-                //dynamic customer = new
-                //{
-                //    id = Guid.NewGuid().ToString(),
-                //    partitionKeyPath = "Mark",
-                //    LastName = "Monurst",
-                //    BirthdayInEpoch = 125566,
-                //    Email = "mark@gmail.com"
-                //};
-
-                var response = await cont.CreateItemAsync(cust);
+                var response = await cont.CreateItemAsync(customer);
                 log.LogInformation("Saved Successfully");
                 return new OkObjectResult("Saved Successfully!");
             }
@@ -60,17 +49,6 @@ namespace AzureFunctionText07152023
                 log.LogInformation("Failed: " + ex.Message);
                 return new BadRequestObjectResult(ex.Message);
             }
-            
-
-            //string name = req.Query["name"];
-
-            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            //dynamic data = JsonConvert.DeserializeObject(requestBody);
-            //name = name ?? data?.name;
-
-            //string responseMessage = string.IsNullOrEmpty(name)
-            //    ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-            //    : $"Hello, {name}. This HTTP triggered function executed successfully.";
             
         }
     }
